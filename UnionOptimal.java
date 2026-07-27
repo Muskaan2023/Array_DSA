af76889df7ec1ae44207b2dc08b87305fcdfbe43
Muskaan2023 <muskaankhatoon2003@gmail.com>
2026-07-27 22:18:51 +0530

    union of sorted arrays

---
+++ UnionOptimal.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class UnionOptimal {
+
+    public static ArrayList<Integer> union(int[] arr1, int[] arr2) {
+
+        int i = 0;
+        int j = 0;
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        while (i < arr1.length && j < arr2.length) {
+
+            if (arr1[i] <= arr2[j]) {
+
+                if (ans.size() == 0 || ans.get(ans.size() - 1) != arr1[i]) {
+                    ans.add(arr1[i]);
+                }
+
+                i++;
+            }
+
+            else {
+
+                if (ans.size() == 0 || ans.get(ans.size() - 1) != arr2[j]) {
+                    ans.add(arr2[j]);
+                }
+
+                j++;
+            }
+        }
+
+        while (i < arr1.length) {
+
+            if (ans.size() == 0 || ans.get(ans.size() - 1) != arr1[i]) {
+                ans.add(arr1[i]);
+            }
+
+            i++;
+        }
+
+        while (j < arr2.length) {
+
+            if (ans.size() == 0 || ans.get(ans.size() - 1) != arr2[j]) {
+                ans.add(arr2[j]);
+            }
+
+            j++;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 2, 2, 3, 4};
+        int[] arr2 = {2, 3, 5};
+
+        ArrayList<Integer> result = union(arr1, arr2);
+
+        System.out.println(result);
+    }
+}
